3086db6a53f041bdf38b4a5601a2d0839fe07d9d
Nicholas Werner <nicholas-werner@outlook.com>
2024-11-20 19:50:39 -0800

    initial commit. api passes the test cases, but does not yet include receipt validation or containerization.

---
+++ app.py
@@ -0,0 +1,64 @@
+import uuid
+import math
+from datetime import date, time
+from flask import Flask, request
+
+app = Flask(__name__)
+
+# As per the requirements, receipt storage is ephemeral. There is no persistence between sessions.
+receipts = {}
+
+
+@app.post("/receipts/process")
+def process_receipt():
+    """
+    Given a valid receipt in the request body, create a new uuid for that receipt and save it in the
+    receipts dict.
+    """
+    id = str(uuid.uuid4())
+    receipts[id] = request.json
+    return {"id": id}, 201
+
+
+@app.route("/receipts/<id>/points")
+def get_points(id):
+    """
+    If a receipt with the given id exists, return its point value according to the following rules:
+        1 point for every alphanumeric character in retailer name
+        50 points if the total is a round dollar amount with no cents
+        25 points if the total is a multiple of 0.25
+        5 points for every two items on the receipt
+        If the trimmed length of the item description is a multiple of 3, multiply the price by 0.2
+            and round up to the nearest integer. The result is the number of points earned.
+        6 points if the day in the purchase date is odd
+        10 points if the time of purchase is after 2:00PM and before 4:00pm.
+    """
+    if not id in receipts:
+        return "id not found", 404
+
+    receipt = receipts[id]
+    retailer = receipt["retailer"]
+    purchase_date = receipt["purchaseDate"]
+    purchase_time = receipt["purchaseTime"]
+    total = float(receipt["total"])
+    items = receipt["items"]
+
+    points = 0
+    points += sum(1 for c in retailer if c.isalnum())
+    if total == round(total):
+        points += 50
+    if total % 0.25 == 0:
+        points += 25
+    points += 5 * (len(items) // 2)
+    for item in items:
+        price = float(item["price"])
+        description = item["shortDescription"].strip()
+        if len(description) % 3 == 0:
+            points += math.ceil(0.2 * price)
+    if date.fromisoformat(purchase_date).day % 2 == 1:
+        points += 6
+    hour = time.fromisoformat(purchase_time).hour
+    if hour >= 14 and hour < 16:
+        points += 10
+
+    return {"points": points}
---
+++ test_app.py
@@ -0,0 +1,91 @@
+import uuid
+from app import app, receipts
+
+# should be 28 points
+target = {
+    "retailer": "Target",
+    "purchaseDate": "2022-01-01",
+    "purchaseTime": "13:01",
+    "items": [
+        {"shortDescription": "Mountain Dew 12PK", "price": "6.49"},
+        {"shortDescription": "Emils Cheese Pizza", "price": "12.25"},
+        {"shortDescription": "Knorr Creamy Chicken", "price": "1.26"},
+        {"shortDescription": "Doritos Nacho Cheese", "price": "3.35"},
+        {
+            "shortDescription": "   Klarbrunn 12-PK 12 FL OZ  ",
+            "price": "12.00",
+        },
+    ],
+    "total": "35.35",
+}
+
+# should be 109 points
+corner_market = {
+    "retailer": "M&M Corner Market",
+    "purchaseDate": "2022-03-20",
+    "purchaseTime": "14:33",
+    "items": [
+        {"shortDescription": "Gatorade", "price": "2.25"},
+        {"shortDescription": "Gatorade", "price": "2.25"},
+        {"shortDescription": "Gatorade", "price": "2.25"},
+        {"shortDescription": "Gatorade", "price": "2.25"},
+    ],
+    "total": "9.00",
+}
+
+
+def test_process_receipt():
+    """
+    when a receipt is posted to /receipts/process
+    then a receipt is created
+        and the response includes a new id
+        and the id is a valid uuid
+    """
+    with app.test_client() as client:
+        response = client.post("/receipts/process", json=target)
+
+        assert response.status_code == 201
+        assert "id" in response.json
+        id = response.json["id"]
+        assert id in receipts
+        assert is_valid_uuid(id)
+
+
+def test_points():
+    """
+    when a receipt is posted to /receipts/process
+        and a request is made to /receipts/<id>/points with its id
+    then the response includes a points value
+        and it's correct for the given payload
+    """
+    with app.test_client() as client:
+        response = client.post("/receipts/process", json=target)
+        id = response.json["id"]
+        response = client.get(f"/receipts/{id}/points")
+        assert response.json == {"points": 28}
+
+        response = client.post("/receipts/process", json=corner_market)
+        id = response.json["id"]
+        response = client.get(f"/receipts/{id}/points")
+        assert response.json == {"points": 109}
+
+
+def test_missing_id():
+    """
+    given a certain id does not exist in receipts
+    when a request is made to /receipts/<id>/points
+    then the response is 404
+    """
+    id = uuid.uuid4()
+    with app.test_client() as client:
+        response = client.get(f"/receipts/{id}/points")
+
+        assert response.status_code == 404
+
+
+def is_valid_uuid(id):
+    try:
+        uuid.UUID(id)
+        return True
+    except ValueError:
+        return False
